2213552b7be7938f5266f17e8b965f516fea14d7
jessicayung <jy357@cam.ac.uk>
2016-03-12 18:20:06 +0000

    First commit: scrapers used to pull data from job descriptions etc that I don't want to have to copy manually.

---
+++ practice-bs4-scraper.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+from urllib2 import urlopen
+from sys import argv
+import re
+
+script, theurl = argv
+
+def job_info(url):
+	html = urlopen(url).read()
+	soup = BeautifulSoup(html, "lxml")
+	job_title = soup.h2.contents
+
+	job_responsibilities = soup.select("h4:nth-of-type(1) div.fcb")
+	pretty_job_responsibilities = []
+	for resp in job_responsibilities:
+		resp = resp.get_text()
+		pretty_job_responsibilities.append(resp)
+
+	job_requirements = soup.find_all("div", class_="fcb")
+	pretty_job_requirements = []
+	for req in job_requirements:
+		req = req.get_text()
+		pretty_job_requirements.append(req)
+
+	print "Job title: {0} \n Responsibilities: {1} \n Requirements: {2}".format(job_title, pretty_job_responsibilities, pretty_job_requirements)
+
+job_info(theurl)
